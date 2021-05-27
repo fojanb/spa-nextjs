2a9ca6d217542a6d09b14ca2a38eaba875c730ae
Fojan Babaali <fozhanmail@gmail.com>
2021-05-26 23:15:54 -0700

    Create about.js

---
+++ spa-nextjs/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from "next/head";
+
+const About = () => {
+    return(
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+            {/* <body> */}
+            <h1>About Section goes here</h1>
+            {/* <body> */}
+        </div>
+    );
+}
+export default About;
