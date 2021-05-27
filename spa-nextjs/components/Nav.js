5f4f5a1416bebc8e2fdf01c4f1f58bcd3c2cbf07
Fojan Babaali <fozhanmail@gmail.com>
2021-05-26 23:44:39 -0700

    Update Nav.js

--- spa-nextjs/components/Nav.js
+++ spa-nextjs/components/Nav.js
@@ -1,5 +1,17 @@
-import navStyles from '../styles/Nav.module.css';
+import navStyles from "../styles/Nav.module.css";
+import Link from "next/link";
 const Nav = () => {
-    return(<nav className={navStyles.nav}></nav>)
-}
+  return (
+    <nav className={navStyles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Nav;
