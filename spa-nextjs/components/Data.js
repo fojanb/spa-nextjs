a7d83fec0967e7b422aeac8adf82efbefb759872
Fojan Babaali <fozhanmail@gmail.com>
2021-05-28 22:42:00 -0700

    Create Data.js

---
+++ spa-nextjs/components/Data.js
@@ -0,0 +1,12 @@
+// import dataStyles from "../styles/Data.module.css";
+// const Data = (props) => {
+//   console.log(props);
+//   return (
+//     <div className={dataStyles.grid}>
+//       {props.entities.map((e,index) => (
+//         <h3 key={index}>{index+1} - {e.title}</h3>
+//       ))}
+//     </div>
+//   );
+// };
+// export default Data;
