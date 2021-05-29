ddebd56b18fe7fd24152a4daacae0ffbdc354c27
Fojan Babaali <fozhanmail@gmail.com>
2021-05-28 22:42:03 -0700

    Update index.js

--- spa-nextjs/pages/index.js
+++ spa-nextjs/pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import Data from "../components/Data";
 // import About from "./about";
 //index.js is index.html actually
-const Home = () => {
+const Home = ({ data }) => {
+  // console.log(data);
   return (
     <div>
       <Head>
@@ -13,18 +15,25 @@ const Home = () => {
         />
       </Head>
       <div>Welcome to Next.js</div>
+      <Data entities={data}/>
+      {/* {data.map(d => <h3>{d.title}</h3>)} */}
       {/* <About /> */}
     </div>
   );
 };
 export default Home;
+// How to fetch data from a API in next.js ?
+// There are three ways to fetch data from API :
+// 1.getStaticProps() -> will fetch data in build time (npm next build) -> (Faster Approach)
+// 2.getServerSideProps() -> will fetch data in each request->(Slower Approach)
+// 3.getStaticPaths() -> For dynamic routing
 
 export const getStaticProps = async () => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=6`
   );
   const data = await response.json();
-  // getStaticProps will return an obect as below :
+  // getStaticProps will return an object as below :
   return {
     props: {
       data,
