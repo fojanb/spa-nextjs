6e8399340fdf109b1bd3cae2144729f1ca32e9a2
Fojan Babaali <fozhanmail@gmail.com>
2021-05-31 13:33:02 -0700

    Update _app.js

--- spa-nextjs/pages/_app.js
+++ spa-nextjs/pages/_app.js
@@ -8,3 +8,24 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// Next.js Agenda :
+// 1-Getting setup with create-react-app
+// 2-Files and Folders
+// 3-Pages and Routing
+// 4-Head
+// 5-Layouts and CSS modules
+// 6-Nav Component and Link
+// 7-Create a Header
+// 8-Styled JSX (Inline Styling)
+// 9-custom Document
+// 10-Data Fetching
+// 11-getStaticProps() ----> Static Generation Mode (Fast) (Default mode in Next.js)
+// 12-Showing Data
+// 13-Nested Routing
+// 14-getServerSideProps() ----> Server Side Rendering Mode (Slow)
+// 15-getstaticPaths() ----> Dynamic path
+// 16-Export a Static Website
+// 17-API Routes
+// 18-Using the API Data
+// 19-Custom Meta Component
