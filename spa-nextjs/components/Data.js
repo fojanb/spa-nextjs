5f1e8eb1bb470f6beeae159aa2e3718f48ecbec0
Fojan Babaali <fozhanmail@gmail.com>
2021-05-31 12:39:10 -0700

    Update Data.js

--- spa-nextjs/components/Data.js
+++ spa-nextjs/components/Data.js
@@ -1,13 +1,22 @@
 import dataStyles from "../styles/Data.module.css";
+import Link from "next/link";
 const Data = (props) => {
-//   console.log(props);
+  // console.log(props);
+  // console.log(props.entities[1].title);
+  let data = props.entities;
   return (
     <div className={dataStyles.grid}>
-      {props.entities.map((e,index) => (
-        <h3 key={index}>{index+1} - {e.title}</h3>
+      {data.map((card, index) => (
+        <Link href="/card/[id]" as={`/card/${card.id}`}>
+          <div className={dataStyles.card}>
+            <h3 key={index}>
+              {index + 1} - {card.title}
+            </h3>
+            <p>{card.body}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
 };
 export default Data;
-// raf -> Shortcut key to create a function component
