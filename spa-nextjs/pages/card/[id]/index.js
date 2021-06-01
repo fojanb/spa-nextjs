9f3eeb7be7d23d6ec8564bae4d58b0377e4749e9
Fojan Babaali <fozhanmail@gmail.com>
2021-06-01 12:40:09 -0700

    Update index.js

--- spa-nextjs/pages/card/[id]/index.js
+++ spa-nextjs/pages/card/[id]/index.js
@@ -1,18 +1,27 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 // next/route Dynamic Route
 const card = () => {
   const router = useRouter();
   const { id } = router.query;
-  return <div>--Card Number {id}--</div>;
+  return (
+    <>
+      <div>--Card Number {id}--</div>
+      <Link href="/">Go Back</Link>
+    </>
+  );
 };
+
 export default card;
 
-// Note that : When a file is added to the pages directory 
+
+
+// Note that : When a file is added to the pages directory
 // it's automatically available as a route.
-// The router will automatically route files 
+// The router will automatically route files
 // named **index** to the root of the directory.
 // ---------------------------------------------------
-// The Next.js router allows you to do client-side route transitions between pages, 
+// The Next.js router allows you to do client-side route transitions between pages,
 // similar to a single-page application.
 // A React component called Link is provided to do this client-side route transition.
 // '/' → pages/index.js
